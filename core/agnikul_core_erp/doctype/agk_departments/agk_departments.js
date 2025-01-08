896254aacfec4f5d67b29133da0a59f5c5caa00b
vignesh-rameshkumar <vignesh_rameshkumar@agnikul.in>
2025-01-08 16:09:33 +0530

    Core Doctypes

---
+++ core/agnikul_core_erp/doctype/agk_departments/agk_departments.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Agnikul Cosmos Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AGK_Departments', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ core/agnikul_core_erp/doctype/agk_departments/agk_departments.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Agnikul Cosmos Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class AGK_Departments(Document):
+	pass
