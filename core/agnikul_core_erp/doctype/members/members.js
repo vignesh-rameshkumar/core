3957b19f065f1f90de1442d820d1989946652647
Kshitijagn1kul <kshitij_kumaravel@agnikul.in>
2025-01-17 15:58:41 +0530

    Adding Childs instead json

---
+++ core/agnikul_core_erp/doctype/members/members.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Agnikul Cosmos Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Members', {
+	// refresh: function(frm) {
+
+	// }
+});
